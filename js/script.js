24f06e98aade3471e430de396cf0cdc309e9adf3
cohara <oharaconor@hotmail.com>
2014-06-10 20:16:34 +0100

    1

---
+++ js/script.js
@@ -0,0 +1,29 @@
+$( document ).on( "pageinit", function( event ) {
+	var panel_html = ''+
+'<ul data-role="listview" data-inset="true">'+
+    '<li><a href="index.html"> <img src="icons/home.png"/> Home</a></li>'+
+	'<li><a href="Intro.html"> <img src="boat2014.jpeg"/> Welcome</a></li>'+
+	'<li><a href="about.html"> <img src="icons/about.png"/> About</a></li>'+
+'<li><a href="events.html"> <img src="icons/anchor.png"/> Programme</a></li>'+
+'<li><a href="entertainment.html"> <img src="icons/music.png"/> Live Entertainment</a></li>'+
+'<li><a href="Event08.html"> <img src="icons/food.png"/> Artisan Food Quarter</a></li>'+
+'<li><a href="cars.html"> <img src="CarLogos/MotorIcon.jpeg"/> Motor Show</a></li>'+
+	'<li><a href="attractions.html"> <img src="icons/attractions.png"/> Tourist Attractions</a></li>'+
+	'<li><a href="food.html"> <img src="icons/food.png"/> Food & Drink</a></li>'+
+	'<li><a href="hotels.html"> <img src="icons/hotel.png"/> Hotels</a></li>'+
+	'<li><a href="contact.html"> <img src="icons/contact.png"/> Contact</a></li>'+
+
+
+
+
+'<!--<li><a href="tour.html"> <img src="icons/Drogheda.png"/> Virtual Tour of Drogheda</a></li>'+
+
+'-->'+
+''+
+'</ul>';
+		
+		$('.panelhtml').html(panel_html);
+		$('.panelhtml ul[data-role="listview"]').listview();
+});
+
+var map;
