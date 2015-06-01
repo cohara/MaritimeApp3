8e9f8d0e49c293b733c6d0c53647d206edc6645c
Conor O'Hara <conor.h.ohara@gmail.com>
2015-06-01 12:17:29 +0100

    1

--- js/script.js
+++ js/script.js
@@ -2,13 +2,13 @@ $( document ).on( "pageinit", function( event ) {
 	var panel_html = ''+
 '<ul data-role="listview" data-inset="true">'+
     '<li><a href="index.html"> <img src="icons/home.png"/> Home</a></li>'+
-	'<li><a href="Intro.html"> <img src="boat2014.jpeg"/> Welcome</a></li>'+
+	'<li><a href="Intro.html"> <img src="boat2015.jpg"/> Welcome</a></li>'+
 	'<li><a href="about.html"> <img src="icons/about.png"/> About</a></li>'+
 '<li><a href="events.html"> <img src="icons/anchor.png"/> Programme</a></li>'+
 
 	'<li><a href="attractions.html"> <img src="icons/attractions.png"/> Tourist Attractions</a></li>'+
 	'<li><a href="food.html"> <img src="icons/food.png"/> Food & Drink</a></li>'+
-	'<li><a href="hotels.html"> <img src="icons/hotel.png"/> Hotels</a></li>'+
+	'<li><a href="Accommodation.html"> <img src="icons/hotel.png"/> Accommodation</a></li>'+
 	'<li><a href="contact.html"> <img src="icons/contact.png"/> Contact</a></li>'+
 
 
